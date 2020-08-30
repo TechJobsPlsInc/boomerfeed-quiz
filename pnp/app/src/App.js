5ec3fefaaadf61475302a444605bbbdc9c5e9c4a
flyingonionman <39283458+flyingonionman@users.noreply.github.com>
2020-08-30 00:49:47 -0400

    Boiler plate code for Springboot + React
    Source : https://developer.okta.com/blog/2018/07/19/simple-crud-react-and-spring-boot
    
    To run :
    1. In the top directory ( pnp ) run
    [mvnw spring-boot:run]
    2. In the app directory [npm start]

---
+++ pnp/app/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+import Home from './components/Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import GroupList from './components/GroupList';
+import GroupEdit from './components/GroupEdit';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route path='/' exact={true} component={Home}/>
+          <Route path='/groups' exact={true} component={GroupList}/>
+          <Route path='/groups/:id' component={GroupEdit}/>
+
+        </Switch>
+      </Router>
+    )
+  }
+}
+
+export default App;
---
+++ pnp/src/main/java/com/cooper/pnp/PnpApplication.java
@@ -0,0 +1,13 @@
+package com.cooper.pnp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PnpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PnpApplication.class, args);
+	}
+
+}
