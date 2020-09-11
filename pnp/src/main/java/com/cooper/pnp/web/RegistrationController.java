d5716505fce17cdedb6e49a82798ba5ee1e2f370
flyingonionman <39283458+flyingonionman@users.noreply.github.com>
2020-09-11 18:03:49 -0400

    Sending email from backend testisng
    Sending emails through Amazon SES

---
+++ pnp/src/main/java/com/cooper/pnp/web/RegistrationController.java
@@ -0,0 +1,76 @@
+package com.cooper.pnp.web;
+
+import javax.mail.MessagingException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cooper.pnp.model.EmailUser;
+import com.cooper.pnp.service.MailService;
+
+/**
+ * This class contains a Mail API developed using Spring Boot
+ * 
+ * @author MukulJaiswal
+ *
+ */
+@RestController
+public class RegistrationController {
+
+	@Autowired
+	private MailService notificationService;
+
+	@Autowired
+	private EmailUser user;
+
+	/**
+	 * 
+	 * @return
+	 */
+	@RequestMapping("send-mail")
+	public String send() {
+
+		/*
+		 * Creating a User with the help of User class that we have declared and setting
+		 * Email address of the sender.
+		 */
+		user.setEmailAddress("minyoungna1997@gmail.com");  //Receiver's email address
+		/*
+		 * Here we will call sendEmail() for Sending mail to the sender.
+		 */
+		try {
+			notificationService.sendEmail(user);
+		} catch (MailException mailException) {
+			System.out.println(mailException);
+		}
+		return "Congratulations! Your mail has been send to the user.";
+	}
+
+	/**
+	 * 
+	 * @return
+	 * @throws MessagingException
+	 */
+	@RequestMapping("send-mail-attachment")
+	public String sendWithAttachment() throws MessagingException {
+
+		/*
+		 * Creating a User with the help of User class that we have declared and setting
+		 * Email address of the sender.
+		 */
+		user.setEmailAddress("minyoungwork1997@gmail.com"); //Receiver's email address
+
+		/*
+		 * Here we will call sendEmailWithAttachment() for Sending mail to the sender
+		 * that contains a attachment.
+		 */
+		try {
+			notificationService.sendEmailWithAttachment(user);
+		} catch (MailException mailException) {
+			System.out.println(mailException);
+		}
+		return "Congratulations! Your mail has been send to the user.";
+	}
+}
